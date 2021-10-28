f76a866ace99b40c936982311329c6b780980b88
EC2 Default User <ec2-user@ip-10-10-56-17.ec2.internal>
2021-10-28 00:20:12 +0000

    update

--- utils.py
+++ utils.py
@@ -83,6 +83,21 @@ colorlist = [
 # Paths 
 
 
+
+### Plotly 
+# Remove 'jupyter_client.kernelspec.KernelSpecManager' from jupyterlab config (~/.jupyter.jupyter_notebook_config.py)
+# #!/bin/bash
+# conda install "ipywidgets=7.5" --yes
+# export NODE_OPTIONS=--max-old-space-size=4096
+# jupyter labextension install @jupyter-widgets/jupyterlab-manager@1.1 --no-build
+# jupyter labextension install jupyterlab-plotly@4.6.0 --no-build
+# jupyter labextension install plotlywidget@4.6.0 --no-build
+# jupyter labextension install jupyterlab-chart-editor --no-build
+# jupyter lab build
+# unset NODE_OPTIONS
+
+
+
 class Credentials(metaclass=ABCMeta):
     pass
     
